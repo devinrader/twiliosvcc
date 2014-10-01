73c1d9a83911fcd0460218cd12aba520dd3d25ca
Devin Rader <devin@twilio.com>
2014-09-30 21:32:35 -0400

    initial commit

---
+++ twiliosvcc/Notification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twiliosvcc
+{
+    public class Notification
+    {
+        public string Message { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Guid { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ twiliosvcc/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+
+namespace twiliosvcc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            JobHost host = new JobHost();
+            host.RunAndBlock();
+        }
+
+        public static async Task SendNotifications( [QueueTrigger("twiliosvcc")] List<Notification> notifications) {
+
+            Console.WriteLine("Starting Bulk Notification Delivery");
+
+            var twilioClient = new TwilioRestClient(ConfigurationManager.AppSettings["ACCOUNTSID"], ConfigurationManager.AppSettings["AUTHTOKEN"]);
+            var amsClient = new MobileServiceClient("", "");
+
+            IMobileServiceTable<Notification> notificationsTable = amsClient.GetTable<Notification>();
+
+            foreach (var notification in notifications)
+            {        
+                //have we sent a notification to this phone number before?
+                //await notificationsTable.Where(n => n.PhoneNumber == notification.PhoneNumber).ToListAsync();
+                
+                //if we have set the flag to send the instructions message
+
+                //save this notification    
+                await notificationsTable.InsertAsync(notification);
+
+                Console.WriteLine("Sending to {0}", notification.PhoneNumber);
+
+                var result = twilioClient.SendMessage(
+                    ConfigurationManager.AppSettings["FROM"], 
+                    notification.PhoneNumber, 
+                    notification.Message,
+                    string.Format("?guid={0}", notification.Guid));
+
+                if (result.RestException != null)
+                {
+                    Console.WriteLine(result.RestException.Message);
+                    notification.Status = "ApiFail";
+                    await notificationsTable.UpdateAsync(notification);
+                }
+                else
+                {
+                    //if first message, send the instructions email
+                }                              
+            }
+        }
+    }
+}
